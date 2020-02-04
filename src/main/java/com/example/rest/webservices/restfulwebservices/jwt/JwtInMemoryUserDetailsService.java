1cbb8f13e3c6f3342fb241ecbec85003d860c513
VitalieLazari <vitalie.lazari@gmail.com>
2020-02-04 17:32:53 +0200

    92 - 91

--- src/main/java/com/example/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
@@ -19,7 +19,7 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
         inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
                 "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
         inMemoryUserList.add(new JwtUserDetails(2L, "ranga",
-                "$2a$10$IetbreuU5KihCkDB6/r1DOJO0VyU9lSiBcrMDT.biU7FOt2oqZDPm", "ROLE_USER_2"));
+                "$2a$10$LBz1jdC/F3YBmx5hKeWg5OYsIbQM3nTcMaFjJEbIw3m9C4OQCHWvK", "ROLE_USER_2"));
 
         //$2a$10$IetbreuU5KihCkDB6/r1DOJO0VyU9lSiBcrMDT.biU7FOt2oqZDPm
     }
